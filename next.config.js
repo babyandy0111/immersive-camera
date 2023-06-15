cba1375ebbdb3204f221527d8209ff19959af3e8
Bambe Chen <bambe.chen@unisys.com>
2023-06-16 00:37:17 +0800

    config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
 const path = require("path");
+const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  basePath: "/immersive-camera",
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/about": { page: "/about" },
+    };
+  },
+  assetPrefix: !debug ? "/immersive-camera/" : "",
   output: "export",
   distDir: "docs",
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  assetPrefix: "./",
 };
