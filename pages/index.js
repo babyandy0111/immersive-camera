91704e6d8406d1151f1f2da96ff844a09de5b2d3
Bambe Chen <bambe.chen@unisys.com>
2023-06-20 03:25:00 +0800

    Add pics

--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,9 @@ export default function Home() {
       >
         <div className={styles.func}>
           <img src={`${process.env.BASE_PATH}` + "/images/logo.png"} />
-          <p>
-            歡迎使用新北市議會
-            <br />類 AR 合照系統
-          </p>
-          <button type="button" onClick={() => router.push("/list/ar")}>Web AR</button>
-          <button type="button" onClick={() => router.push('/list/mix')}>選擇自拍</button>
+          <p>第四屆新北市議會<br />議員合影系統</p>
+          <button type="button" onClick={() => router.push("/list/ar")}>現場合影系統</button>
+          <button type="button" onClick={() => router.push('/list/mix')}>自拍合成系統</button>
         </div>
       </main>
     </Layout>
