08171e4e72fff2869ebd6fd0a98ffdbd9fc41b67
Bambe Chen <bambe.chen@unisys.com>
2023-07-07 11:29:43 +0800

    new 10 councilor

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,6 @@ export default function Home() {
           <img src={`${process.env.BASE_PATH}` + "/images/logo.png"} />
           <p>第四屆新北市議會<br />議員合影系統</p>
           <button type="button" onClick={() => router.push("/list/ar")}>現場合影系統</button>
-          <button type="button" onClick={() => router.push('/list/mix')}>自拍合成系統</button>
         </div>
       </main>
     </Layout>
